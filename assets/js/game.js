b70612e51ee154352039813b7bc6adbe43881e55
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-09 18:11:38 -0500

    random fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,42 +81,47 @@ var fightOrSkip = function() {
 
 
 var fight = function(enemy) {
+    var isPlayerTurn = true;
+    if (Math.random() > 0.5){
+        isPlayerTurn = false;
+    }
     while(playerInfo.health > 0 && enemy.health > 0) {
-        if (fightOrSkip()) {
-            break;
-        }
-            // if player choses to fight, then fight
-            
-                var damagePlayer = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-                enemy.health = Math.max(0, enemy.health - damagePlayer);
-                console.log (
-                    playerInfo.name+" attacked "+enemy.name+". "+enemy.name+" now has "+enemy.health+" health remainig."
-                );
-                if (enemy.health <= 0){
-                    window.alert(enemy.name+" has died!");
-                    playerInfo.money = playerInfo.money +20;
-                    console.log("playerInfo.money", playerInfo.money)
-                    break;
-                }
-                    else {
-                        window.alert(enemy.name+" still has "+enemy.health+" health left.");
+        if (isPlayerTurn) {
+            if (fightOrSkip()) {
+                break;
+            }                
+                    var damagePlayer = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+                    enemy.health = Math.max(0, enemy.health - damagePlayer);
+                    console.log (
+                        playerInfo.name+" attacked "+enemy.name+". "+enemy.name+" now has "+enemy.health+" health remainig."
+                    );
+                    if (enemy.health <= 0){
+                        window.alert(enemy.name+" has died!");
+                        playerInfo.money = playerInfo.money +20;
+                        console.log("playerInfo.money", playerInfo.money)
+                        break;
                     }
-                    var damageEnemy = randomNumber(enemy.attack - 3, enemy.attack);
-                    playerInfo.health = Math.max(0, playerInfo.health - damageEnemy);
-                    console.log(
-                        enemy.name+" attacked "+playerInfo.name+". "+playerInfo.name+" now has "+playerInfo.health+" health remaining."
-                );
-                if (playerInfo.health <= 0) {
-                    window.alert(playerInfo.name+" has died!");
-                    break;
-                }
-                    else {
-                        window.alert(playerInfo.name+" still has "+playerInfo.health+" health left.");
+                        else {
+                            window.alert(enemy.name+" still has "+enemy.health+" health left.");
+                        }
+        } else {
+                        var damageEnemy = randomNumber(enemy.attack - 3, enemy.attack);
+                        playerInfo.health = Math.max(0, playerInfo.health - damageEnemy);
+                        console.log(
+                            enemy.name+" attacked "+playerInfo.name+". "+playerInfo.name+" now has "+playerInfo.health+" health remaining."
+                    );
+                    if (playerInfo.health <= 0) {
+                        window.alert(playerInfo.name+" has died!");
+                        break;
                     }
+                        else {
+                            window.alert(playerInfo.name+" still has "+playerInfo.health+" health left.");
+                        }
+                }
+                isPlayerTurn = !isPlayerTurn;
             }
-            // else {
-            //     window.alert("You need to choose a valid option. Try again!");
-            // }   
+                // else {
+                //     window.alert("You need to choose a valid option. Try again!");
 };
 
 
