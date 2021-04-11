972707eeb1cb33832d45ab8cd1141c08af36a54a
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-10 22:27:52 -0500

    get the high score using localStorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -56,7 +56,7 @@ var enemyInfo = [
         name: "Robo Trumble",
         attack: randomNumber(10,14)
     }
-]
+];
 
 
 
@@ -77,7 +77,7 @@ var fightOrSkip = function() {
         }
     }
     return false;
-}
+};
 
 
 var fight = function(enemy) {
@@ -150,11 +150,26 @@ var startGame = function() {
 
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
-        if (playerInfo.health > 0) {
-            window.alert("Great job, you've survived the game! You now have a score of "+playerInfo.money+".");
-        } else {
-            window.alert("You've lost your robot in battle.");
+        var highScore = localStorage.getItem("highscore");
+        if (highScore === null) {
+            highScore = 0;
         }
+        // highScore = highScore || 0;
+
+        if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        } else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
+        
+        // if (playerInfo.health > 0) {
+        //     window.alert("Great job, you've survived the game! You now have a score of "+playerInfo.money+".");
+        // } else {
+        //     window.alert("You've lost your robot in battle.");
+        // }
+
     var playAgainConfirm = window.confirm("Would you like to play again?");
         if (playAgainConfirm) {
             startGame();
